5a4eb7122a9953772a1aad03bf561b3f9e786ce0
Tobias <tobiasdirksen@users.noreply.github.com>
2020-06-20 12:46:10 +0200

    Update hs110-data-collect.py

--- hs110-data-collect.py
+++ hs110-data-collect.py
@@ -1,11 +1,12 @@
 import sys
-import time
 import socket
 import json
-import threading
+from struct import pack
+from datetime import datetime
+from pandas.io.json import json_normalize
+import os
 
 
-# https://stackoverflow.com/questions/21017698/converting-int-to-bytes-in-python-3
 def int_to_bytes(x):
     return x.to_bytes((x.bit_length() + 7) // 8, 'big')
 
@@ -13,81 +14,79 @@ def int_to_bytes(x):
 def int_from_bytes(xbytes):
     return int.from_bytes(xbytes, 'big')
 
+# Python 3.x version
+if sys.version_info[0] > 2:
+	def encrypt(string):
+		key = 171
+		result = pack('>I', len(string))
+		for i in string:
+			a = key ^ ord(i)
+			key = a
+			result += bytes([a])
+		return result
 
-# Based on: https://github.com/softScheck/tplink-smartplug/blob/master/tplink-smartplug.py
-def encrypt(string):
-    key = 171
-    result = b"\0\0\0\0"
-    for i in string:
-        a = key ^ i
-        key = a
-        result += int_to_bytes(a)
-    return result
+	def decrypt(string):
+		key = 171
+		result = ""
+		for i in string:
+			a = key ^ i
+			key = i
+			result += chr(a)
+		return result
 
+# Python 2.x version
+else:
+	def encrypt(string):
+		key = 171
+		result = pack('>I', len(string))
+		for i in string:
+			a = key ^ ord(i)
+			key = a
+			result += chr(a)
+		return result
 
-def decrypt(string):
-    key = 171
-    result = b""
-    for i in string:
-        a = key ^ i
-        key = i
-        result += int_to_bytes(a)
-    return result
+	def decrypt(string):
+		key = 171
+		result = ""
+		for i in string:
+			a = key ^ ord(i)
+			key = ord(i)
+			result += chr(a)
+		return result
 
 
 def send_hs_command(address, port, cmd):
     data = b""
-
     tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         tcp_sock.connect((address, port))
         tcp_sock.send(encrypt(cmd))
-        data = tcp_sock.recv(2048)
+        data = tcp_sock.recv(1024)
     except socket.error:
-        print("Socket closed.", file=sys.stderr)
+        raise ValueError("Socket closed")
     finally:
         tcp_sock.close()
     return data
 
-
-def store_metrics(current, voltage, power):
-    current_time = time.time()
-
-    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run(address):
+"""Input ip address of HS110"""
     try:
-        tcp_sock.connect(("localhost", 2003))
-        tcp_sock.send("hs110-tv.voltage {0} {1} \n".format(voltage, current_time).encode())
-        tcp_sock.send("hs110-tv.current {0} {1} \n".format(current, current_time).encode())
-        tcp_sock.send("hs110-tv.power {0} {1} \n".format(power, current_time).encode())
-    except socket.error:
-        print("Unable to open socket on graphite-carbon.", file=sys.stderr)
-    finally:
-        tcp_sock.close()
+        timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        timestamp_utc = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+        
+        device_info = send_hs_command(address, 9999, b'{"system":{"get_sysinfo":null}}')
+        decrypted_device_info = decrypt(device_info[4:]).decode()
+        json_device_info = json.loads(decrypted_device_info)['system']['get_sysinfo']
+        
+        data = send_hs_command(address, 9999, b'{"emeter":{"get_realtime":{}}}')
+        decrypted_data = decrypt(data[4:]).decode()
+        json_data = json.loads(decrypted_data)
+        
+        df = json_normalize(json_data["emeter"]["get_realtime"])
+        df['timestamp'] = timestamp
+        df['timestamp_utc'] = timestamp_utc
+        df['location'] = json_device_info['alias']
+        df['rssi'] = json_device_info['rssi']
 
-
-def run():
-    threading.Timer(15.0, run).start()
-
-    data = send_hs_command("192.168.1.4", 9999, b'{"emeter":{"get_realtime":{}}}')
-
-    if not data:
-        print("No data returned on power request.", file=sys.stderr)
-        store_metrics(0, 0, 0)
-        return
-
-    decrypted_data = decrypt(data[4:]).decode()
-    json_data = json.loads(decrypted_data)
-    emeter = json_data["emeter"]["get_realtime"]
-
-    if not emeter:
-        print("No emeter data returned on power request.", file=sys.stderr)
-        store_metrics(0, 0, 0)
-        return
-
-    store_metrics(emeter["current"], emeter["voltage"], emeter["power"])
-
-    print("Stored values, current: {0}, voltage: {1}, power: {2}".format(
-        emeter["current"], emeter["voltage"], emeter["power"]))
-
-
-run()
+    except Exception as error:
+        print(error)
